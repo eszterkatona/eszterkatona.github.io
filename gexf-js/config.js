fa45d1fbccdbaa3102ed2ae0147643d3670c875f
Eszter Katona <ekatona@precognox.com>
2018-01-10 20:31:12 +0100

    update categories

--- gexf-js/config.js
+++ gexf-js/config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "szines3.gexf",
+    graphFile : "ujkat.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
