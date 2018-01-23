90486c3f8e84a85cfd9a1d35d1fe23435216de81
Eszter Katona <ekatona@precognox.com>
2018-01-23 22:33:41 +0100

    version 10

--- gexf-js/config.js
+++ gexf-js/config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "v1.gexf",
+    graphFile : "v3.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
