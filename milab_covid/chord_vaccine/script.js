7d771fcbcc3644d6da6757c98c48186c3bfd1fb1
Eszter Katona <katona.eszter.rita@gmail.com>
2021-06-18 15:47:28 +0200

    update chord diagram

--- milab_covid/chord_vaccine/script.js
+++ milab_covid/chord_vaccine/script.js
@@ -2,9 +2,9 @@
 //////////////////////// Set-Up ////////////////////////////
 ////////////////////////////////////////////////////////////
 
-var margin = {left:2, top:0, right:2, bottom:0},
-	width = Math.min(window.innerWidth, 400) - margin.left - margin.right,
-    height = Math.min(window.innerWidth, 400) - margin.top - margin.bottom,
+var margin = {left:2, top:2, right:2, bottom:2},
+	width = Math.min(window.innerWidth, 300) - margin.left - margin.right,
+    height = Math.min(window.innerWidth, 300) - margin.top - margin.bottom,
     innerRadius = Math.min(width, height) * .3,
     outerRadius = innerRadius * 1.05;
 	
@@ -115,7 +115,7 @@ outerArcs.append("path")
 outerArcs.append("text")
     .each(function(d) { d.angle = (d.startAngle + d.endAngle) / 2; })
     .attr("dy", ".35em")
-    .style("font-size", "12px")
+    .style("font-size", "10px")
     .style("fill", "gray")
 
   .attr("class", "titles")
